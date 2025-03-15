d11eaff5fbd75563138f9321a88077d7d7911cfc
khang <tranminhkhang376@gmail.com>
2025-03-15 09:43:11 +0700

    khang xong ảnh

--- src/main.py
+++ src/main.py
@@ -10,7 +10,8 @@ screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Virtual Laboratory")
-
+bg = pygame.image.load("src/assets/image/phonghoahoc.png")
+screen.blit(bg, (0, 0))
 # Main game loop
 running = True
 while running:
@@ -21,7 +22,7 @@ while running:
     # Game logic for mixing chemicals and triggering reactions would go here
 
     # Clear the screen
-    screen.fill((255, 255, 255))
+    # screen.fill(())
 
     # Update the display
     pygame.display.flip()
